836fe3b86bb80d54ed33e83a6734306f7056a97c
ShadowArcher289 <ahasuncion21@gmail.com>
2025-12-15 20:24:10 -0500

    updated .json file format and implemented the schedule to place items in individual timeslots. Bug: each item can only be in one timeslot, which would mean only one teacher teaching at a given time which does not make sense

--- stp-scheduler-frontend/app/page.tsx
+++ stp-scheduler-frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import localData from "../data/SchedulerData.json";
 import Section from "./sectionCard"
 
-const times = [
+const times = [ // unused
     { id: 0, hour: '08', timeOfDay: "am"},
     { id: 1, hour: '09', timeOfDay: "am"},
     { id: 2, hour: '10', timeOfDay: "am"},
@@ -12,15 +12,45 @@ const times = [
     { id: 7, hour: '03', timeOfDay: "pm"},
     { id: 8, hour: '04', timeOfDay: "pm"},
     { id: 9, hour: '05', timeOfDay: "pm"},
-  ]
+  ];
+
+  /**
+   * The number of sections in the grid.
+   */
+  var SectionCount = localData.sections.length;
+
+  /**
+   * Converts time from military time(ex: 13:00) to civilian time(ex: 1:00pm)
+   * @param time string
+   * @returns string
+   */
+  function militaryToCivilianTime(time: string): string{
+    var splitTime = time.split(":");
+    var suffix = "am";
+    if(parseInt(splitTime[0]) > 12){
+      splitTime[0] = (parseInt(splitTime[0]) - 12).toString();
+      suffix = "pm";
+    }
+    return splitTime.join(":") + suffix;
+  }
+
+  /**
+   * Returns the number of empty spaces in the grid.
+   * @returns number
+   */
+  function getEmptySpacesCount(): number{
+    return 41 - SectionCount;
+  }
 
 export default function Home() {
+  SectionCount = localData.sections.length;
   return (
     // table to put all the sections in the day
     // Sections to put in each table that require data to display
-    <section className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div id="schedule" className="grid grid-cols-[1fr,2fr,2fr,2fr,2fr,2fr] grid-rows-10 grid-flow-dense w-auto border-2 border-dotted">
+    <section className="flex min-h-screen items-center justify-center p-12 bg-zinc-50 font-sans dark:bg-black">
+      <div id="schedule" className="grid grid-cols-6 grid-rows-12 grid-flow-dense w-auto border-2 border-solid">
         
+        {/* Headers */}
         <h4 className="col-start-1 col-span-1 bg-stone-300 text-black border-2 border-white">Time</h4>
         <h4 className="col-start-2 col-span-1 bg-stone-300 text-black border-2 border-white">Monday</h4>
         <h4 className="col-start-3 col-span-1 bg-stone-300 text-black border-2 border-white">Tuesday</h4>
@@ -28,79 +58,19 @@ export default function Home() {
         <h4 className="col-start-5 col-span-1 bg-stone-300 text-black border-2 border-white">Thursday</h4>
         <h4 className="col-start-6 col-span-1 bg-stone-300 text-black border-2 border-white">Friday</h4>
 
-        {times.map(time => (
-          <div key={time.id} className="p-3 col-start-1 col-span-1 bg-stone-300 text-black border-2 border-white">{time.hour}:00{time.timeOfDay}</div>
+        {/* Fill in the time on the left */}
+        {localData.timeBlock.map(time => (
+          <div key={time.id} className="p-3 col-start-1 col-span-1 bg-stone-300 text-black border-2 border-white">{militaryToCivilianTime(time.start)} - {militaryToCivilianTime(time.end)}</div>
         ))}
-        {/* <div className="col-start-1 col-span-1">8:00am</div>
-        <div className="col-start-1 col-span-1">9:00am</div>
-        <div className="col-start-1 col-span-1">10:00am</div>
-        <div className="col-start-1 col-span-1">11:00am</div>
-        <div className="col-start-1 col-span-1">12:00am</div>
-        <div className="col-start-1 col-span-1">1:00pm</div>
-        <div className="col-start-1 col-span-1">2:00pm</div>
-        <div className="col-start-1 col-span-1">3:00pm</div>
-        <div className="col-start-1 col-span-1">4:00pm</div>
-        <div className="col-start-1 col-span-1">5:00pm</div> */}
 
+        {/* Fill in sections */}
         {localData.sections.map((section, index) => (
           <Section key={index} id={section.id} subject={section.subject} level={section.level} timeBlockId={section.timeBlockId} days={section.days} studentIds={section.studentIds} teacherId={section.teacherId}></Section>
         ))}
 
-        {/* 
-        <div className="col-start-2 col-span-1">8:00am</div>
-        <div className="col-start-2 col-span-1">9:00am</div>
-        <div className="col-start-2 col-span-1">10:00am</div>
-        <div className="col-start-2 col-span-1">11:00am</div>
-        <div className="col-start-2 col-span-1">12:00am</div>
-        <div className="col-start-2 col-span-1">1:00pm</div>
-        <div className="col-start-2 col-span-1">2:00pm</div>
-        <div className="col-start-2 col-span-1">3:00pm</div>
-        <div className="col-start-2 col-span-1">4:00pm</div>
-        <div className="col-start-2 col-span-1">5:00pm</div>
-
-        <div className="col-start-3 col-span-1">8:00am</div>
-        <div className="col-start-3 col-span-1">9:00am</div>
-        <div className="col-start-3 col-span-1">10:00am</div>
-        <div className="col-start-3 col-span-1">11:00am</div>
-        <div className="col-start-3 col-span-1">12:00am</div>
-        <div className="col-start-3 col-span-1">1:00pm</div>
-        <div className="col-start-3 col-span-1">2:00pm</div>
-        <div className="col-start-3 col-span-1">3:00pm</div>
-        <div className="col-start-3 col-span-1">4:00pm</div>
-        <div className="col-start-3 col-span-1">5:00pm</div>
-
-        <div className="col-start-4 col-span-1">8:00am</div>
-        <div className="col-start-4 col-span-1">9:00am</div>
-        <div className="col-start-4 col-span-1">10:00am</div>
-        <div className="col-start-4 col-span-1">11:00am</div>
-        <div className="col-start-4 col-span-1">12:00am</div>
-        <div className="col-start-4 col-span-1">1:00pm</div>
-        <div className="col-start-4 col-span-1">2:00pm</div>
-        <div className="col-start-4 col-span-1">3:00pm</div>
-        <div className="col-start-4 col-span-1">4:00pm</div>
-        <div className="col-start-4 col-span-1">5:00pm</div>
-
-        <div className="col-start-5 col-span-1">8:00am</div>
-        <div className="col-start-5 col-span-1">9:00am</div>
-        <div className="col-start-5 col-span-1">10:00am</div>
-        <div className="col-start-5 col-span-1">11:00am</div>
-        <div className="col-start-5 col-span-1">12:00am</div>
-        <div className="col-start-5 col-span-1">1:00pm</div>
-        <div className="col-start-5 col-span-1">2:00pm</div>
-        <div className="col-start-5 col-span-1">3:00pm</div>
-        <div className="col-start-5 col-span-1">4:00pm</div>
-        <div className="col-start-5 col-span-1">5:00pm</div>
-
-        <div className="col-start-6 col-span-1">8:00am</div>
-        <div className="col-start-6 col-span-1">9:00am</div>
-        <div className="col-start-6 col-span-1">10:00am</div>
-        <div className="col-start-6 col-span-1">11:00am</div>
-        <div className="col-start-6 col-span-1">12:00am</div>
-        <div className="col-start-6 col-span-1">1:00pm</div>
-        <div className="col-start-6 col-span-1">2:00pm</div>
-        <div className="col-start-6 col-span-1">3:00pm</div>
-        <div className="col-start-6 col-span-1">4:00pm</div>
-        <div className="col-start-6 col-span-1">5:00pm</div> */}
+        {Array.from({ length: getEmptySpacesCount() }, (_, index) => (
+          <div key={index} className="col-span-1 row-span-1 border-2 border-dotted p-6 text-center"></div>
+        ))}
       </div>
       
     </section>
--- stp-scheduler-frontend/app/sectionCard.tsx
+++ stp-scheduler-frontend/app/sectionCard.tsx
@@ -1,3 +1,4 @@
+import { time } from "console";
 import localData from "../data/SchedulerData.json";
 
 interface SectionProps {
@@ -5,11 +6,16 @@ interface SectionProps {
     subject: string, 
     level: string, 
     timeBlockId: number, 
-    days: string, 
+    days: string[], 
     studentIds: Array<number>, 
     teacherId: number
 }
 
+/**
+ * returns a background color based on the subject of the section
+ * @param subject string
+ * @returns string "valid css color"
+ */
 function getBackgroundColor(subject: string){
     switch (subject.toLowerCase()) {
         case "math":
@@ -24,6 +30,46 @@ function getBackgroundColor(subject: string){
     }
 }
 
+/**
+ * Returns the column number that corresponds to a given day
+ * @param day string (ex: 'M' for Monday, 'W' for Wednesday, 'R' for Thursday)
+ * @returns number
+ */
+function getStartColumn(day: string): number{
+    var column = 7; // the default column is, column 7, the buffer
+
+    switch (day) { // set the column based on the given day
+        case "M":
+            column = 2;
+            break;
+        case "T":
+            column = 3;
+            break;
+        case "W":
+            column = 4;
+            break;
+        case "R":
+            column = 5;
+            break;
+        case "F":
+            column = 6;
+            break;
+        default:
+            console.log("getStartColumn in sectionCard.tsx: failed to calculate column");
+            break;
+    }
+    return column;
+}
+
+/**
+ * Returns the respective row number for a given timeBlock.
+ * @param timeBlockId id of the timeBlock for a given section
+ * @returns number
+ */
+function getStartRow(timeBlockId: number){
+    return timeBlockId + 2;
+}
+
 export default function Section({
     id, 
     subject, 
@@ -34,13 +80,22 @@ export default function Section({
     teacherId
 }: SectionProps){
     return(
-        <div 
-            className="col-span-1 row-span-1 border-2 border-dotted p-6 text-center"
-            style={{
-                backgroundColor: getBackgroundColor(subject)
-            }}
-        >
-            {localData.teachers[teacherId].name} - {subject}
-        </div>
+        <>
+            {days.map((day: string, index) => (
+                <div 
+                    key={index}
+                    className="col-span-1 row-span-1 border-2 border-dotted p-6 text-center"
+                    style={{
+                        backgroundColor: getBackgroundColor(subject),
+                        gridColumnStart: getStartColumn(day),
+                        gridColumnEnd: `span 1`,
+                        gridRowStart: getStartRow(timeBlockId),
+                        gridRowEnd: `span 1`
+                    }}
+                >
+                    {localData.teachers[teacherId].name} - {subject}
+                </div>
+            ))}
+        </>
     );
 }
